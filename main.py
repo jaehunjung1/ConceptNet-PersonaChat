490e020467e5e18345110c80aec06f1efa5fb588
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-08-31 00:01:43 +0900

    q-a match computation

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import ipdb
 import json
 from tqdm import tqdm
 from openie.open_ie_api import *
+from nltk import word_tokenize
 
+# Torch device
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
@@ -29,26 +31,33 @@ class ConceptNet:
         # self.ent1.build_vocab(self.dataset, min_freq=1)
         # self.ent2.build_vocab(self.dataset, min_freq=1)
 
-        iter = list(data.Iterator(self.dataset, len(self.dataset), device=device, repeat=False,
-                                  sort_key=lambda x: len(x.ent1), train=False))[0]
+        iterator = list(data.Iterator(self.dataset, len(self.dataset), device=device, repeat=False,
+                                  sort_key=lambda x: -self.ENT1.vocab.stoi[x], train=False))[0]
 
-        self.rel, self.ent1, self.ent2 = iter.rel, iter.ent1, iter.ent2
+        self.rel, self.ent1, self.ent2 = iterator.rel.unsqueeze(1).to(device), iterator.ent1.unsqueeze(1).to(device), \
+                                         iterator.ent2.unsqueeze(1).to(device)
 
 
 class Persona:
-    def __init__(self, dir):
+    def __init__(self, dir, concepetNet):
         self.path = dir
+        self.conceptNet = conceptNet
         with open(self.path, 'r') as f:
             self.dataset = json.load(f)
+        self.dialog = []  # list of chunked q-a lists, each word_tokenized
+        self.q, self.a = None, None
         self.process_json()
 
     def process_json(self):
         for i, row in tqdm(enumerate(self.dataset)):
             # split multi turn dialogs into list of q-a pairs
             dialog = row['dialog']
-            self.dataset[i]['dialog'] = [(dialog[i]['text'], dialog[i + 1]['text']) for i in range(len(dialog) - 1)]
+            self.dataset[i]['dialog'] = [(dialog[i]['text'].lower(), dialog[i + 1]['text'].lower())
+                                         for i in range(len(dialog) - 1)]
+            self.dialog.extend([list((map(word_tokenize, self.dataset[i]['dialog'][j])))
+                                for j in range(len(dialog)-1)])
 
-            # # remove needless k-v pairs
+            # remove needless k-v pairs
             self.dataset[i].pop('start_time')
             self.dataset[i].pop('end_time')
             self.dataset[i].pop('eval_score')
@@ -57,12 +66,35 @@ class Persona:
             self.dataset[i].pop('participant2_id')
 
             # 'user_profile', 'bot_profile': persona
-            self.dataset[i]['user_profile'] = call_api_many(row['user_profile'], pagination_param=10000)
+            # self.dataset[i]['user_profile'] = call_api_many(row['user_profile'], pagination_param=10000)
 
-    # def extract_entity(self):
+        field = data.Field(sequential=True, batch_first=True)
+        field.vocab = conceptNet.ENT1.vocab
+
+        q, a = tuple(zip(*self.dialog))
+        self.q = field.numericalize(field.pad(q)).to(device)
+        self.a = field.numericalize(field.pad(a)).to(device)
 
 
 if __name__ == "__main__":
+    print("Device:", device)
+
     conceptNet = ConceptNet("./conceptnet_data/train100k.tsv")
-    # persona = Persona("./persona_data/data_tolokers.json")
+    persona = Persona("./persona_data/data_tolokers.json", conceptNet)
+
+    qa_match = 0  # number of (entity1, entity2) in q-a pair
+    for i in tqdm(range(persona.q.size(0))):
+
+        row_q = persona.q[i]
+        row_a = persona.a[i]
+
+        ent_q = torch.unique(row_q)  # unique entities in query
+        ent_a = torch.unique(row_a)  # unique entities in answer
+        ent_q = ent_q.repeat(conceptNet.ent1.size(0), 1)
+        ent_a = ent_a.repeat(conceptNet.ent2.size(0), 1)
+
+        qa_match += sum([torch.sum((conceptNet.ent1 == ent_q) * (conceptNet.ent2 == ent_a)[:, i].unsqueeze(1))
+                         for i in range(ent_a.size(1))])
+
+    qa_avg = qa_match / persona.q.size(0)
     ipdb.set_trace()
